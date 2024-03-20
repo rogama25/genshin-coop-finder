52863152d076ff683e9b42f49804f05a4a6b24dd
Rodrigo Garcia Martin <rodrigogarciama@gmail.com>
2024-03-20 20:20:22 +0100

    Added basic page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,21 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
+import "./globals.css"
+import { NavBar } from "@/app/navbar";
 
 export const metadata: Metadata = {
     title: "Genshin co-op finder",
     description: "Find teams to farm coop resources or bosses"
 };
 
-export default function RootLayout({children}: Readonly<{
+export default function RootLayout({ children }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
-        <body>{children}</body>
+            <body className="bg-blue-950">
+                <NavBar />
+                {children}
+            </body>
         </html>
     );
 }
---
+++ src/app/navbar.tsx
@@ -0,0 +1,7 @@
+export function NavBar() {
+    return (
+        <div className="shadow-pink-900 drop-shadow-xl h-20 bg-orange-500">
+
+        </div>
+    )
+}
